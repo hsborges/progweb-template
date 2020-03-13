66ea8ee9859720119c91bd20d0cb2b7634aa710f
gabcostab4 <gabriel.silva@before.com.br>
2020-03-13 13:34:50 -0400

    inicia algoritmo para preencher posicoes

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 let counter = 0;
 
-function createSquare() {
+function createSquare(filled) {
   let square = document.createElement("div");
+
   square.innerText = "";
+
+  if (filled) {
+    square.innerText = "x";
+  }
   square.className = "square";
-  square.addEventListener('click', squareClicked)
-  square.id = counter++
+  square.addEventListener("click", squareClicked);
+  square.id = counter++;
 
   return square;
+}
+
+function populateSquares() {
+  let start = {
+    row: Math.floor(Math.random() * Math.floor(100)),
+    col: Math.floor(Math.random() * Math.floor(100))
+  };
+
+  let end = {
+    row: Math.floor(Math.random() * Math.floor(100)),
+    col: Math.floor(Math.random() * Math.floor(100))
+  };
+
+  /** Posição inválida: Mesmas coordenadas */
+  if (start.row === end.row && start.col === end.row) {
+    return false;
+  }
+  /** Posição inválida: Coordenada diagonal*/
+  if (start.row !== end.row && start.col !== end.row) {
+    return false;
+  }
+
+  return [start, end];
 }
 
 function createBoard() {
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
+      let populate = populateSquares()
+      if(populate && populate[0].row === i && populate[0].col === j) {
+        root.appendChild(createSquare(true));
+        console.log('filled');
+
+        continue;
+      }
       root.appendChild(createSquare());
     }
   }
@@ -21,4 +56,5 @@ function createBoard() {
 function squareClicked(e) {
   console.log(e.target.id)
 }
+
 window.onload = createBoard();
