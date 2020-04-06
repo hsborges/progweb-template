673bf368b109f1cb3cecf4f7e3355fbf3267f4d2
Lucasucas <lucasoliveira96@outlook.com>
2020-04-06 00:38:32 -0400

    Add e Delete Recipe

--- backend/src/Controllers/RecipeController.js
+++ backend/src/Controllers/RecipeController.js
@@ -5,9 +5,28 @@ module.exports = {
         const { name, recipe, image, video, category_id } = request.body
 
         try {
-
-        } catch () {
-
+            const [id] = await connection('recipes').insert({
+                name,
+                recipe,
+                image,
+                video,
+                category_id
+            })
+            return response.json({ id })
+        } catch (err) {
+            return response.json({ error: "Não foi possível adicionar receita" })
         }
+    },
+
+    async index(request, response) {
+        const categories = await connection('recipes').select('*')
+        return response.json(categories)
+    },
+
+    async delete(request, response) {
+        const { id } = request.params
+
+        await connection('recipes').where('id', id).delete()
+        return response.status(204).send()
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = express.Router()
 const UserController = require('./Controllers/UserController')
 const CategoryController = require('./Controllers/CategoryController')
+const RecipeController = require('./Controllers/RecipeController')
 
 
 routes.post('/signin', UserController.create)
@@ -12,7 +13,9 @@ routes.post('/categories', CategoryController.create)
 routes.get('/categories', CategoryController.index)
 routes.delete('/categories/:id', CategoryController.delete)
 
-
+routes.post('/recipes', RecipeController.create)
+routes.get('/recipes', RecipeController.index)
+routes.delete('/recipes/:id', RecipeController.delete)
 
 
 
