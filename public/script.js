e80f35b028d688d91361aa142e65ac5cb627ce4d
Cleriston Nantes Petrikic <arteefx2012@gmail.com>
2020-04-20 05:55:47 -0400

    Inserindo navios de forma aleatoria

--- public/script.js
+++ public/script.js
@@ -1,117 +1,327 @@
-let ships  = [
-  {
-    title: 'submarino',
-    size: 2,
-    quantity: 4,
-    coordinates: []
-  },
+let forbidden  = {
+    type: 'forbidden'
+};
 
-  {
-    title: 'contra-torpedos',
-    size: 3,
-    quantity: 3,
-    coordinates: []
-  },
+let field = createField();
 
+let shipStruct = [
   {
-    title: 'navio-tanque',
-    size: 4,
-    quantity: 2,
-    coordinates: []
+    part: [
+      {
+        type: 'porta-avioes',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'porta-avioes',
+        position: '1',
+        status: 'ok'
+      },
+      {
+        type: 'porta-avioes',
+        position: '2',
+        status: 'ok'
+      },
+      {
+        type: 'porta-avioes',
+        position: '3',
+        status: 'ok'
+      },
+      {
+        type: 'porta-avioes',
+        position: '4',
+        status: 'ok'
+      }
+    ]
   },
-
   {
-    title: 'porta-avioes',
-    size: 5,
-    quantity: 1,
-    coordinates: []
+    part: [
+      {
+        type: 'navio-tanque',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'navio-tanque',
+        position: '1',
+        status: 'ok'
+      },
+      {
+        type: 'navio-tanque',
+        position: '2',
+        status: 'ok'
+      },
+      {
+        type: 'navio-tanque',
+        position: '3',
+        status: 'ok'
+      }
+    ]
   },
+  {
+    part: [
+      {
+        type: 'navio-tanque',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'navio-tanque',
+        position: '1',
+        status: 'ok'
+      },
+      {
+        type: 'navio-tanque',
+        position: '2',
+        status: 'ok'
+      },
+      {
+        type: 'navio-tanque',
+        position: '3',
+        status: 'ok'
+      }
+    ]
+  },
+  {
+    part: [
+      {
+        type: 'contra-torpedos',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'contra-torpedos',
+        position: '1',
+        status: 'ok'
+      },
+      {
+        type: 'contra-torpedos',
+        position: '2',
+        status: 'ok'
+      }
+    ]
+  },
+  {
+    part: [
+      {
+        type: 'contra-torpedos',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'contra-torpedos',
+        position: '1',
+        status: 'ok'
+      },
+      {
+        type: 'contra-torpedos',
+        position: '2',
+        status: 'ok'
+      }
+    ]
+  },
+  {
+    part: [
+      {
+        type: 'contra-torpedos',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'contra-torpedos',
+        position: '1',
+        status: 'ok'
+      },
+      {
+        type: 'contra-torpedos',
+        position: '2',
+        status: 'ok'
+      }
+    ]
+  },
+  {
+    part: [
+      {
+        type: 'submarino',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'submarino',
+        position: '1',
+        status: 'ok'
+      }
+    ]
+  },
+  {
+    part: [
+      {
+        type: 'submarino',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'submarino',
+        position: '1',
+        status: 'ok'
+      }
+    ]
+  },
+  {
+    part: [
+      {
+        type: 'submarino',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'submarino',
+        position: '1',
+        status: 'ok'
+      }
+    ]
+  },
+  {
+    part: [
+      {
+        type: 'submarino',
+        position: '0',
+        status: 'ok'
+      },
+      {
+        type: 'submarino',
+        position: '1',
+        status: 'ok'
+      }
+    ]
+  }
 ]
 
-function createSquare(i, j) {
-  let square = document.createElement("div");
+function createField(){
+  let arr = new Array(10);
 
-  square.innerText = "";
+  for (let i = 0; i < 10; i++){
+    arr[i] = new Array(10);
+  }
 
-  square.className = "square";
-  square.addEventListener("click", squareClicked);
-  square.id = [i, j].join('');
-
-  return square;
+  return arr;
 }
 
-function createBoard() {
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      root.appendChild(createSquare(i, j));
+function createSquare(i, j) {
+    let square = document.createElement('div');
+  
+    square.innerText = '';
+  
+    square.className = 'square';
+    square.addEventListener('click', squareClicked);
+    square.id = [i, j].join('');
+  
+    return square;
+  }
+  
+  function createBoard() {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        root.appendChild(createSquare(i, j));
+      }
     }
   }
-}
+  
+  function squareClicked(e) {
+    let node = document.getElementById(this.id);
+    //node.style.backgroundColor = 'black';
+    
+  }
 
-function squareClicked(e) {
-  let { id } = e.target;
-  ships.map((ship) => {
-    for(const coord of ship.coordinates){
-      try {
-        let node = document.getElementById(coord.join(''));
-        node.style.backgroundColor = "black";
-      }
-      catch(e) {
-        console.error(e.message)
+  function validatePosition(i, j, orientation, size){ // checks if the position is valid.
+    let isValid = true;
+    if(orientation){
+      for(i; i < size && isValid; i++){
+        if(field[i][j]!= undefined){
+          isValid = false;
+        }
       }
     }
-  });
-}
-
-/* Dont need this */
-function getStartingPoint(previous = ['', '']) {
-  return new Promise((resolve, reject) => {
-    let y = random(9, 0);
-    let x = random(9, 0);
-
-    while (x === previous[0] && y === previous[1]) {
-      y = random(9, 0);
-      x = random(9, 0);
+    else{
+      for(i; i < size && isValid; i++){
+        if(field[j][i]!= undefined){
+          isValid = false;
+        }
+      }
     }
+    
+    return isValid;
+  }
 
-    resolve([x, y])
-  })
-}
+  function setShipsPositions(){
+    for(let ship of shipStruct){
+      let x, y, orientation;
+      do{
+        x = random(0, 9 - ship.part.length);
+        y = random(0, 9);
+        orientation = random(0, 1); // horizontal if is 0, and vertical if is 1.
+      }while(!validatePosition(x, y, orientation, ship.part.length + x));
 
-function setShipsPositions() {
-  let prevStartingPoints = [0, 0]
-  for (let ship of ships) {
-    for(let i = 0; i < ship.quantity; i++) {
-      let direction = Math.round(Math.random()) === 0 ? "vertical" : "horizontal"; // 0 - Vertical, 1 - Horizontal
-      getStartingPoint(prevStartingPoints).then((startPoint) => {
-        prevStartingPoints.push(startPoint)
-        x = startPoint[0]
-        y = startPoint[1]
-
-        for (let j = 0; j < ship.size; j++) {
-          if (direction === "vertical") {
-            y = (y + 1) % 10;
-            ship.coordinates.push([x, y]);
-          } else {
-            x = (x + 1) % 10
-            ship.coordinates.push([x, y]);
+      if(orientation){
+        if(x > 0){
+          field[x-1][y] = forbidden;
+        }
+        if(x+ship.part.length < 10){
+          field[x + ship.part.length][y] = forbidden;
+        }
+        for(let i = 0; i < ship.part.length; i++){
+          field[x+i][y] = ship.part[i];
+          if(y > 0){
+            field[x+i][y-1] = forbidden;
+          }
+          if(y < 9){
+            field[x+i][y+1] = forbidden;
           }
         }
-      })
+      }
+      else{
+        if(x > 0){
+          field[y][x-1] = forbidden;
+        }
+        if(x+ship.part.length < 10){
+          field[y][x + ship.part.length] = forbidden;
+        }
+        for(let i = 0; i < ship.part.length; i++){
+          field[y][x+i] = ship.part[i];
+          if(y > 0){
+            field[y-1][x+i] = forbidden;
+          }
+          if(y < 9){
+            field[y+1][x+i] = forbidden;
+          }
+        }
+      }
+      for(let i = 0; i < ship.part.length; i++){
+        console.log(ship.part[i]);
+      }
     }
   }
-}
 
-/**
- *
- * @param {number} max Inclusive max value
- * @param {number} min
- */
-function random(max, min) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  function mapShips(){
+    for(let i = 0; i < field.length; i++){
+      for(let j = 0; j < field[i].length; j++){
+        if(field[i][j] != undefined){
+          let sqr = document.getElementById([i, j].join(''));
+          sqr.style.backgroundColor = 'black';
+        }
+        if(field[i][j] == forbidden){
+          let sqr = document.getElementById([i, j].join(''));
+          sqr.style.backgroundColor = 'gray';
+        }
+      }
+    }
+  }
 
-window.onload = (function () {
+
+  function random(min, max) {
+    return Math.floor(Math.random() * (min + max + 1) - min);
+  }
+
   createBoard();
   setShipsPositions();
-})();
+  mapShips();
