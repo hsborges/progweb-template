4be14f054e7d7830b91abdcf08216171806dee1c
Denner Vidal <denner.vidal@gmail.com>
2020-04-24 20:36:14 -0400

    Add react-router

--- src/Components/Home/Categories.js
+++ src/Components/Home/Categories.js
@@ -19,6 +19,7 @@ export const Categories = () => {
     alignItems: "center",
     flexDirection: "column",
   };
+
   return (
     <div
       style={{
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './root.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./root.css";
+import { Router, Route } from "react-router";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App} exact={true} />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
