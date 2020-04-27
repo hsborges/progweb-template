138f3636595a49e9d4911a809dd7fef304747c07
Jean Ortiz <jeanortiz@brbyte.com>
2020-04-26 20:37:50 -0400

    Padding on Product page

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Paper, Typography, ButtonBase, Container, Divider, Button } from '@material-ui/core';
+import { Grid, Typography, ButtonBase, Container, Divider, Button } from '@material-ui/core';
 import ProductSpec from './ProductSpec';
 import { green } from '@material-ui/core/colors';
 import SellerInfo from './SellerInfo';
 import Icon from "@mdi/react";
 import {
-  mdiCart 
+  mdiCart
 } from "@mdi/js";
 
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: 'auto',
+    padding: '12px'
   },
   image: {
     width: 512,
@@ -56,63 +53,61 @@ export const ProductDetails = () => {
   const classes = useStyles();
 
   return (
-    <Container>
-      <div className={classes.root}>
-        <Paper className={classes.paper}>
-          <Container>
-            <Grid item xs>
-              <Typography gutterBottom variant="h5">
-                LG Smart TV LG 49" 4K
+    <Container className={classes.root}>
+      <div>
+        <Container>
+          <Grid item xs>
+            <Typography gutterBottom variant="h5">
+              LG Smart TV LG 49" 4K
             </Typography>
+          </Grid>
+        </Container>
+        <Divider style={{ marginTop: "12px" }} />
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item>
+                <ButtonBase className={classes.image}>
+                  <img className={classes.img} alt="" src="https://www.lg.com/br/images/tv/md06134556/gallery/N01_d01.jpg" />
+                </ButtonBase>
+              </Grid>
             </Grid>
-          </Container>
-          <Divider style={{ marginTop: "12px" }} />
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm container>
-              <Grid item xs container direction="column" spacing={2}>
+            <Grid item xs style={{ marginTop: "24px" }}>
+              <Grid container direction="column" justify="center" alignItems="flex">
                 <Grid item>
-                  <ButtonBase className={classes.image}>
-                    <img className={classes.img} alt="" src="https://www.lg.com/br/images/tv/md06134556/gallery/N01_d01.jpg" />
-                  </ButtonBase>
-                </Grid>
-              </Grid>
-              <Grid item xs style={{ marginTop: "24px" }}>
-                <Grid container direction="column" justify="center" alignItems="flex">
-                  <Grid item>
-                    <Typography gutterBottom variant="h6">
-                      Vendedor
+                  <Typography gutterBottom variant="h6">
+                    Vendedor
                     </Typography>
-                    <SellerInfo />
-                  </Grid>
-                  <Typography gutterBottom variant="h6" style={{ marginTop: "24px" }}>
-                    Especificações
+                  <SellerInfo />
+                </Grid>
+                <Typography gutterBottom variant="h6" style={{ marginTop: "24px" }}>
+                  Especificações
                 </Typography>
-                  <Grid item>
-                    <ProductSpec />
-                  </Grid>
-                  <Grid item style={{ marginTop: "12px" }}>
-                    <Typography variant="h6">
-                      R$ 1.500,00
+                <Grid item>
+                  <ProductSpec />
+                </Grid>
+                <Grid item style={{ marginTop: "12px" }}>
+                  <Typography variant="h6">
+                    R$ 1.500,00
                 </Typography>
-                  </Grid>
-                  <Grid item style={{ marginTop: "12px" }}>
-                    <Button variant="contained" color="white" className={classes.button} startIcon={<Icon path={mdiCart } size={1.5} />}>Comprar</Button>
-                  </Grid>
+                </Grid>
+                <Grid item style={{ marginTop: "12px" }}>
+                  <Button variant="contained" color="white" className={classes.button} startIcon={<Icon path={mdiCart} size={1.5} />}>Comprar</Button>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Divider />
-          <Typography variant="h6" style={{ marginTop: "12px" }}>
-            Descrição
+        </Grid>
+        <Divider />
+        <Typography variant="h6" style={{ marginTop: "12px" }}>
+          Descrição
           </Typography>
-          <Divider />
-          <Grid item>
-            <Typography variant="body1" style={{ marginTop: "12px" }}>
-              HDR Ativo, NanoCell para cores mais precisas, inteligência artificial ThinQ AI, imersão de som com DTS Virtual.
+        <Divider />
+        <Grid item>
+          <Typography variant="body1" style={{ marginTop: "12px" }}>
+            HDR Ativo, NanoCell para cores mais precisas, inteligência artificial ThinQ AI, imersão de som com DTS Virtual.
             </Typography>
-          </Grid>
-        </Paper>
+        </Grid>
       </div>
     </Container>
   );
