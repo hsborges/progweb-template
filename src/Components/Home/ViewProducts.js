1681f6b692cb07bd7fb46cc3018cc25918eaeeba
Denner Vidal <denner.vidal@gmail.com>
2020-04-26 20:10:12 -0400

    Creating separated css

--- src/Components/Home/ViewProducts.js
+++ src/Components/Home/ViewProducts.js
@@ -2,16 +2,31 @@ import Typography from "@material-ui/core/Typography";
 import { ProductCard } from "./ProductCard";
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  rootDiv: {
+    margin: "24px",
+  },
+  title: {
+    marginBottom: "12px",
+  },
+  displayFlex: {
+    display: "flex",
+  },
+}));
 
 export const ViewProducts = ({ title, products }) => {
+  const classes = useStyles();
+
   return (
-    <div style={{ margin: "24px" }}>
-      <Typography style={{ marginBottom: "12px" }} variant="h5">
+    <div className={classes.rootDiv}>
+      <Typography className={classes.title} variant="h5">
         {title}
       </Typography>
-      <Grid container style={{ display: "flex" }}>
+      <Grid container className={classes.displayFlex}>
         {products.map((item) => (
-          <Grid item xs={6} sm={3} style={{ display: "flex" }}>
+          <Grid item xs={6} sm={3} className={classes.displayFlex}>
             <ProductCard data={item} key={item.title} />
           </Grid>
         ))}
