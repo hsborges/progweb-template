5b0e1073ff7713c94c094b496cdc188cbaab7e42
Denner Vidal <denner.vidal@gmail.com>
2020-05-24 17:52:58 -0400

    Tratativa de erros

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -5,24 +5,33 @@ const User = mongoose.model("User");
 
 module.exports = {
   async show(req, res) {
-    const user = await User.findOne({ nickname: req.params.nick }).select(
-      "-password"
-    );
+    try {
+      const user = await User.findOne({ nickname: req.params.nick }).select(
+        "-password"
+      );
 
-    return res.json(user);
+      return res.json(user);
+    } catch (e) {
+      return res.json(e);
+    }
   },
 
   async update(req, res) {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
 
-    return res.json(user);
+      return res.json(user);
+    } catch (e) {
+      return res.json(e);
+    }
   },
 
   async store(req, res) {
     try {
       const user = await User.create(req.body);
+
       return res.json(user);
     } catch (e) {
       return res.json(e);
@@ -36,10 +45,14 @@ module.exports = {
       if (isValid) {
         const token = uuidv1();
         const tokenUpdate = await user.update({ lastToken: token });
-        if (tokenUpdate.ok)
+
+        if (tokenUpdate.ok) {
           return res.json({ token: `${token}.${user.id}`, isValid });
+        }
+
         return res.json({ error: "Error saving token" });
       }
+
       return res.json({ error: "Invalid password" });
     } catch (e) {
       return res.json(e);
@@ -49,8 +62,13 @@ module.exports = {
   async logout(req, res) {
     try {
       const user = await User.findOne({ email: req.body.email });
-      user.update({ lastToken: null }).then(() => {});
-      return res.json({ message: "Logout successful!" });
+      const tokenUpdate = user.update({ lastToken: null });
+
+      if (tokenUpdate.ok) {
+        return res.json({ message: "Logout successful" });
+      }
+
+      return res.json({ error: "Error removing token" });
     } catch (e) {
       return res.json(e);
     }
