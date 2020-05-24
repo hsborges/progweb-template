df4db8627d1c7774d7480a73fff9e4300002a369
ericcarickli <ericca.rickli>
2020-05-24 00:00:04 -0400

    cor alterada novamente e algumas coisinhas reajustadas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,35 +24,35 @@
       <a href="home.html">Home</a>
     </div>
     <div class="sub-bar">
-      <div class="description-text"><strong>
-        <br>
-        PreservSign, um clube de assinaturas <br />
-        para você que se ama e preserva
-      </strong></div>
-      <br><br>
-      <a href="" class="myButton">Preserve-se!</a>
-    </div>
-    <div class="plan-footer">
-      <div class="basic-plan">
-        <div class="plan-title"><strong>Plano Básico</strong></div>
-        <div class="plan-description">Receba 15 camisinhas em qualquer lugar!</div>
-        <div class="plan-description">Por apenas R$14,99</div>
-        <div class="button-space"><a href="" class="myButton">Mais Detalhes!</a></div>
-      </div>
-      
-      <div class="premium-plan">
-        <div class="plan-title"><strong>Plano Premium</strong></div>
-        <div class="plan-description">Receba 20 camisinhas <br> + lubrificante surpresa <br> em qualquer lugar!</div>
-        <div class="plan-description">Por apenas R$24,99</div>
-        <div class="button-space"><a href="" class="myButton">Mais Detalhes!</a></div>
+  <div class="description-text"><strong>
+      <br>
+      PreservSign, um clube de assinaturas <br />
+      para você que se ama e preserva
+    </strong></div>
+  <br><br>
+  <a href="" class="myButton">Preserve-se!</a>
+</div>
+<div class="plan-footer">
+  <div class="basic-plan">
+    <div class="plan-title"><strong>Plano Básico</strong></div>
+    <div class="plan-description-basic">Receba 15 camisinhas em qualquer lugar!</div>
+    <div class="plan-description-basic">Por apenas R$14,99</div>
+    <div class="button-space"><a href="" class="myButton">Mais Detalhes!</a></div>
+  </div>
 
-      </div>
-      <div class="exxxtra-plan">
-        <div class="plan-title"><strong>Plano Exxxtra</strong></div>
-        <div class="plan-description">Receba 20 camisinhas de sua preferência <br> + lubrificante surpresa <br> + sex toy surpresa diretamente <br> em qualquer lugar!</div>
-        <div class="plan-description">Por apenas R$59,99</div>
-        <div class="button-space-exxxtra"><a href="" class="myButton">Mais Detalhes!</a></div>
-      </div>
-    </div>
-  </body>
+  <div class="premium-plan">
+    <div class="plan-title"><strong>Plano Premium</strong></div>
+    <div class="plan-description">Receba 20 camisinhas <br> + lubrificante surpresa <br> em qualquer lugar!</div>
+    <div class="plan-description">Por apenas R$24,99</div>
+    <div class="button-space-premium"><a href="" class="myButton">Mais Detalhes!</a></div>
+
+  </div>
+  <div class="exxxtra-plan">
+    <div class="plan-title"><strong>Plano Exxxtra</strong></div>
+    <div class="plan-description">Receba 20 camisinhas de sua preferência <br> + lubrificante surpresa <br> + sex toy surpresa diretamente <br> em qualquer lugar!</div>
+    <div class="plan-description">Por apenas R$59,99</div>
+    <div class="button-space-exxxtra"><a href="" class="myButton">Mais Detalhes!</a></div>
+  </div>
+</div>
+</body>
 </html>
