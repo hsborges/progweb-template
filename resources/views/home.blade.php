c18d3533a60d9800dd8da57b68a7dcdd8ce63613
jardimguilherme <guilhermejardim@live.com>
2020-06-06 17:59:40 -0400

    bugs fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,47 +1,12 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>PreservSign</title>
-    <link rel="stylesheet" href="styles/styles.css"/>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2&family=Cookie&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-  <body>
-    
-    <div class="topnav">
-      <div class="logo">
-          <a href="home.html">PreservSign</a>
-      </div>
-      
-      <a href="">Login</a>
-      <a href="">Contato</a>
-      <a href="plans.html">Planos</a>
-      <a href="about.html">Sobre</a>
-      <a href="home.html">Home</a>
-    </div>
-    <div class="sub-bar">
-  <div class="description-text"><strong>
-      <br>
-      PreservSign, um clube de assinaturas <br />
-      para você que se ama e preserva
-    </strong></div>
-  <br><br>
-  <a href="" class="myButton">Preserve-se!</a>
-</div>
-<div class="plan-footer">
-  <div class="basic-plan">
-    <div class="plan-title"><strong>Plano Básico</strong></div>
-    <div class="plan-description-basic">Receba 15 camisinhas em qualquer lugar!</div>
-    <div class="plan-description-basic">Por apenas R$14,99</div>
-    <div class="button-space"><a href="" class="myButton">Mais Detalhes!</a></div>
-  </div>
-
-=======
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>PreservSign</title>
+  <link rel="stylesheet" href="styles/styles.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2&family=Cookie&display=swap" rel="stylesheet" />
+</head>
 @extends('layouts.app')
 
 @section('content')
@@ -49,18 +14,13 @@
     <div class="img-background">
         <img src="img/body-img.jpg" class="image-home">
     </div>
-
-<<<<<<< HEAD
     <div class="center-box-blured">
         <div class="box-title">
             <span>PreservSign</span>
         </div>
-
         <div class="box-message">
-            <span>O cuidado contigo e seu parceiro(a) nunca <br> foi tão importante. Você pode receber camisinhas <br> exclusivas e muito mais na 
-            segurança <br> e discrição da sua casa!</span>
+            <span>O cuidado com você e seu parceiro (a) nunca foi tão importante. Você pode receber camisinhas exclusivas e muita mais na segurança e descrição da sua casa!</span>
         </div>
-
         <div class="box-join-button">
             <div class="text-button">
                 <a href="#">EU QUERO!</a>
@@ -70,32 +30,3 @@
 
 </div>
 @endsection
-=======
->>>>>>> origin/back
-  <div class="premium-plan">
-    <div class="plan-title"><strong>Plano Premium</strong></div>
-    <div class="plan-description">Receba 20 camisinhas <br> + lubrificante surpresa <br> em qualquer lugar!</div>
-    <div class="plan-description">Por apenas R$24,99</div>
-<<<<<<< HEAD
-    <div class="button-space-premium"><a href="" class="myButton">Mais Detalhes!</a></div>
-=======
-    <div class="button-space"><a href="" class="myButton">Mais Detalhes!</a></div>
->>>>>>> origin/back
-
-  </div>
-  <div class="exxxtra-plan">
-    <div class="plan-title"><strong>Plano Exxxtra</strong></div>
-    <div class="plan-description">Receba 20 camisinhas de sua preferência <br> + lubrificante surpresa <br> + sex toy surpresa diretamente <br> em qualquer lugar!</div>
-    <div class="plan-description">Por apenas R$59,99</div>
-    <div class="button-space-exxxtra"><a href="" class="myButton">Mais Detalhes!</a></div>
-  </div>
-</div>
-</body>
-<<<<<<< HEAD
-</html>
-=======
-
-</html>
-@endsection
->>>>>>> origin/back
->>>>>>> origin/front
