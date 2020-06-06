2ec4d0a2b59fef448d639cd0a440279832ce9120
unknown <ricok041098@gmail.com>
2020-06-06 18:25:15 -0400

    Alterada tela de contato

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,16 +6,30 @@
         <img src="img/body-img.jpg" class="image-home">
     </div>
 
-    <div class="right-box-blured-contact">
+    <div class="right-box-blured">
         <div class="box-title">
             <span>Contato</span>
         </div>
 
         <div class="box-message-contact">
-            <span>Telefone: 0800-SE-PRESERVE<br><br>e-mail: preserve@preserve.com</span>
+            <span>Telefone: 0800-SE-PRESERVE<br><br><br>E-mail: preserve@preserve.com</span>
         </div>
 
     </div>
+    <div class="left-box-blured">
+        <div class="box-title">
+            <span>Mensagem</span>
+        </div>
+
+        
+        <div class="container">
+            <input type="text" id="email" name="email" placeholder="Seu E-mail">
+
+            <textarea id="subject" name="subject" placeholder="Sua Mensagem" style="height:100px"></textarea>
+
+            <input type="submit" value="Enviar">
+</div>
+
 
 </div>
 @endsection
