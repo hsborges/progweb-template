e993fe49a3bb77c88bba62029f11fcb4191f172f
gabcostasilva <gabcostadeveloper@gmail.com>
2020-06-26 20:53:24 -0400

    fix: usuario atual nao eh mostrado na lista de online

--- src/events/online.js
+++ src/events/online.js
@@ -1,16 +1,25 @@
+let redis = require('../config/redis')
+
 exports.use = (io) => {
     const online = require('../controller/online');
-    
-    io.on("connection", client => {
+
+    io.on("connection", async client => {
         const username = client.handshake.session.user;
         client.join(username);
+
+        try {
+            redis.set(username, client.id)
+        }catch(err) {
+            console.error(err.message)
+        }
+
         if (!online.check(username)) {
             client.broadcast.emit('newUser', username);
             console.log(`User online: ${username}`);
         }
         online.set(username);
 
-        client.emit('listOnline', online.list());
+        client.emit('listOnline', online.list().filter(user => user !== username));
 
         client.on("disconnect", () => {
             online.remove(username);
